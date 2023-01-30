d7084f8d016dbebabd705b0d66a1467115bd5f2e
Francesco <francesco.donini@iit.cnr.it>
2023-01-30 13:23:52 +0100

    Changed the ipfsMiniApi initialization to default

--- src/ipfs.js
+++ src/ipfs.js
@@ -2,7 +2,16 @@
 
 import ipfsApiMini from 'ipfs-mini';
 
-export const ipfsMini = new ipfsApiMini({ host: 'ipfs.infura.io', port: 5001, protocol: 'https' });
+// export const ipfsMini = new ipfsApiMini({ host: '127.0.0.1', port: 5001, protocol: 'http',base: '/api/v0'  });
+
+/*
+ * When new ipfsApiMini it's call without
+ * arguments it will be initialized with the default parameters
+ *
+ * default { host: '127.0.0.1', port: 5001, protocol: 'http',base: '/api/v0'  }
+ *
+ */
+export const ipfsMini = new ipfsApiMini();
 
 // connection to local IPFS daemon
 // export const IPFS  = ipfsApi('/ip4/127.0.0.1/tcp/5001');
